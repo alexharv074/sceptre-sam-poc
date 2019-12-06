066d590aaa12b2db58e77f50fc2f3d294b65ea7d
Alex Harvey <alexharv074@gmail.com>
2019-12-06 19:04:02 +1100

    Initial commit
    Add the venv.sh and .gitignore.

---
+++ venv.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+usage() {
+  echo "Usage: . $0 [-h]"
+  exit 1
+}
+[ "$1" == "-h" ] && usage
+[ "$0" == "${BASH_SOURCE[0]}" ] && usage
+
+virtualenv venv
+. venv/bin/activate
+pip install sceptre
