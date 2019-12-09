3d34eda3f7c135255c5d62981852bc922bfefb21
Alex Harvey <alexharv074@gmail.com>
2019-12-09 15:52:41 +1100

    Minor refactor in templates/hello.py

--- templates/hello.py
+++ templates/hello.py
@@ -5,15 +5,18 @@ import os
 import yaml
 import json
 
+S3_BUCKET = 'alexharvey3118'
+
 def sceptre_handler(sceptre_user_data):
     os.chdir('sam')
-    subprocess.run(['sam', 'build'])
-    subprocess.run(['sam', 'package',
-        '--s3-bucket', 'alexharvey3118',
-        '--output-template-file', '/tmp/packaged.yaml'])
+    build = "sam build"
+    package = "sam package --s3-bucket %s \
+--output-template-file /tmp/packaged.yaml" % S3_BUCKET
+    subprocess.run(build.split(" "))
+    subprocess.run(package.split(" "))
     stream = open('/tmp/packaged.yaml', 'r')
     with open('/tmp/packaged.yaml') as f:
         return json.dumps(yaml.load(f, Loader=yaml.FullLoader))
 
 if __name__ == '__main__':
-    print(sceptre_handler('foo'))
+    print(sceptre_handler('dummy'))
