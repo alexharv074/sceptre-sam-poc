536539a484d91e211671e4baf1a8a21516c6d142
Alex Harvey <alexharv074@gmail.com>
2019-12-13 11:49:51 +1100

    Refactor further improvements

--- templates/hello.py
+++ templates/hello.py
@@ -5,15 +5,7 @@ import os
 import yaml
 import json
 
-S3_BUCKET = 'alexharvey3118'
-
 def sceptre_handler(sceptre_user_data):
-    os.chdir('sam')
-    build = "sam build"
-    package = "sam package --s3-bucket %s \
---output-template-file /tmp/packaged.yaml" % S3_BUCKET
-    subprocess.run(build.split(" "))
-    subprocess.run(package.split(" "))
     stream = open('/tmp/packaged.yaml', 'r')
     with open('/tmp/packaged.yaml') as f:
         return json.dumps(yaml.load(f, Loader=yaml.FullLoader))
